b311e94a886151d174302c77bd7bc0c2d9156e0a
jhonattan <jhonattan.benitez@hotmail.com>
2021-02-08 15:05:51 -0500

    eliminar citas

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,5 +1,6 @@
 import React, {Fragment, useState} from "react";
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
   //Arreglo de citas 
@@ -14,6 +15,12 @@ function App() {
     ]);
   }
    
+  //Función que elimina una cita por su id
+  const eliminarCita = id =>{
+    const nuevasCitas = citas.filter(cita  => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
@@ -25,7 +32,14 @@ function App() {
            />
            </div> 
            <div className="one-half column"> 
-           
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita 
+                key={cita.id}
+               cita={cita}
+               eliminarCita={eliminarCita}
+              />
+            ))}
            </div> 
 
          </div>
