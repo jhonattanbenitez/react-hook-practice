9a36a5240275906e102b982eba25cdcba9eadb78
jhonattan <jhonattan.benitez@hotmail.com>
2021-02-08 14:27:05 -0500

    reiniciar formulario

--- citas/src/components/Formulario.jsx
+++ citas/src/components/Formulario.jsx
@@ -48,6 +48,13 @@ const Formulario = ({crearCita}) => {
     crearCita(cita);
 
     //Reiniciar el form
+    actualizarCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: "",
+    })
   };
   return (
     <Fragment>
