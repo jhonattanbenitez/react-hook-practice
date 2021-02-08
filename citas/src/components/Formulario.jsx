ff0e7dd1495cae41196ccd184c9e3010a8e79385
jhonattan <jhonattan.benitez@hotmail.com>
2021-02-08 11:49:38 -0500

    creando el state y la función que modifica el state y los valores de cada campo del formulario

--- citas/src/components/Formulario.jsx
+++ citas/src/components/Formulario.jsx
@@ -1,20 +1,79 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState } from "react";
 
 const Formulario = () => {
-    return ( 
-        <Fragment>
-            <h2>Crear Cita</h2>
-            <form action="">
-                <label htmlFor="">Nombre Mascota</label>
-                <input 
-                    type="text"
-                    name="mascota"
-                    className="u-full-width"
-                    placeholder="nombre mascota"
-                />
-            </form>
-        </Fragment>
-      );
-}
- 
+  // crear State de citas
+
+    const [cita, actualizarCita] = useState({
+    mascota: '',
+    propietario: '',
+    fecha:'',
+    hora:'',
+    sintomas: ''
+  });
+
+  //función que se ejectua cada vez que el usuario escribe algo
+  const actualizarState = (e) => {
+    actualizarCita ({
+      ...cita,
+      [e.target.name] : e.target.value
+    })
+  };
+  //Extraer los valores de los campos del formulario 
+  const {mascota, propietario, fecha, hora, sintomas} = cita;
+
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+      <form action="">
+        <label htmlFor="">Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="nombre mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+        <label htmlFor="">Nombre del dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="nombre del dueño de la mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
+        <label htmlFor="">Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+        <label htmlFor="">Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+        <label htmlFor="">Síntomas </label>
+        <textarea
+          type="text"
+          name="sintomas"
+          className="u-full-width"
+          placeholder="¿Cuál es el problema con su mascota?"
+          onChange={actualizarState}
+          value={sintomas}
+        />
+        <button type="submit" className="u-full-width button-primary">
+          Agregar Cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
 export default Formulario;
