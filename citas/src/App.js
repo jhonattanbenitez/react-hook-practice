255028c19f8c0d0da1ab276f1d967c38f302dba2
jhonattan <jhonattan.benitez@hotmail.com>
2021-02-09 09:37:30 -0500

    agregando useEffect para guardar en el localstorage

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,10 +1,18 @@
-import React, {Fragment, useState} from "react";
+import React, {Fragment, useState, useEffect} from "react";
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
   //Arreglo de citas 
   const [citas, guardarCitas] = useState([]); 
+
+  //Use effect para realizar ciertas operaciones cuando el state cambia
+   useEffect(() => {
+     console.log("listo");
+    /*  return () => {
+       cleanup
+     } */
+   }, [citas])
 
   //función que modifique las citas actuales y agregue una nueva
 
@@ -20,7 +28,8 @@ function App() {
     const nuevasCitas = citas.filter(cita  => cita.id !== id);
     guardarCitas(nuevasCitas);
   }
-
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
@@ -32,7 +41,7 @@ function App() {
            />
            </div> 
            <div className="one-half column"> 
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita => (
               <Cita 
                 key={cita.id}
