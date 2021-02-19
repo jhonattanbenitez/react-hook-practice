6599e1f4455b24d0ffc99800565482a0839da08c
jhonattan <jhonattan.benitez@hotmail.com>
2021-02-19 14:50:09 -0500

    container and button added

---
+++ breaking-bad/src/components/button/button.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+    background-size: 300px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #FFF;
+    margin-top: 3rem;
+    padding: 1rem 3rem;
+    font-size: 2rem;
+    border: 2px solid black;
+    text-transform: uppercase;
+
+`;
+
+
