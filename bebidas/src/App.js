ee660466e5de56722b3fda21f5c2f868c7204d21
jhonattan <jhonattan.benitez@hotmail.com>
2021-03-29 11:42:50 -0500

    first commit bebidas

---
+++ bebidas/src/App.js
@@ -0,0 +1,19 @@
+import React,{Fragment} from 'react';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <div className="container mt-5">
+        <div className="row">
+          
+        </div>
+      </div>
+    </Fragment>
+  
+  );
+}
+
+export default App;
---
+++ bebidas/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return ( <header>
+        <h1>Buscador de recetas de bebidas</h1>
+        </header> );
+}
+ 
+export default Header;
